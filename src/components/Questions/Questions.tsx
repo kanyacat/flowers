aa56f96d99028e58cdb05750971e3ff2bfba0f9d
Kan <kanyakancat@gmail.com>
2023-11-25 13:38:17 +0300

    questions

---
+++ src/components/Questions/Questions.tsx
@@ -0,0 +1,44 @@
+import styles from './Questions.module.scss'
+import Container from '../Container/Container'
+import { Link } from 'react-router-dom'
+
+const Questions = () => {
+	return (
+		<div className={styles.root}>
+			<span className={styles.leaves} />
+			<span className={styles.have} />
+			<div className={styles.back}>
+				<Container>
+					<div>
+						<div className={styles.head}>
+							<h1 className={styles.title}>
+								остались <span>вопросы?</span>
+							</h1>
+							<p className={styles.text}>
+								<span className={styles.line} />
+								Отправьте ваш вопрос, заказ, предложение или жалобу через форму
+								обратной связи, и наш специалист свяжется с вами в течение 15
+								минут.
+							</p>
+						</div>
+						<div className={styles.form}>
+							<form action='submit'>
+								<input type='text' placeholder={'Ваше имя'} />
+								<input type='tel' placeholder={'+7 (977) 777-77-77'} />
+								<input type='text' placeholder={'Ваш комментарий'} />
+								<button type={'submit'}>отправить</button>
+								<p>
+									Нажимая на кнопку «Отправить», я даю свое согласие на
+									обработку персональных данных, в соответствии с{' '}
+									<Link to={'/'}>Политикой конфиденциальности</Link>
+								</p>
+							</form>
+						</div>
+					</div>
+				</Container>
+			</div>
+		</div>
+	)
+}
+
+export default Questions
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -4,6 +4,7 @@ import Catalogue from '../../components/Catalogue/Catalogue'
 import Popular from '../../components/Popular/Popular'
 import HowDoOrder from '../../components/HowDoOrder/HowDoOrder'
 import Special from '../../components/Special/Special'
+import Questions from '../../components/Questions/Questions'
 
 const Main = () => {
 	return (
@@ -13,6 +14,7 @@ const Main = () => {
 			<Popular />
 			<HowDoOrder />
 			<Special />
+			<Questions />
 		</div>
 	)
 }
