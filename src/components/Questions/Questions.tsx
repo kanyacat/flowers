bad6872ce3067e3fc965f9a0a93cf466039bbe5e
Kan <kanyakancat@gmail.com>
2023-11-26 13:53:52 +0300

    inst

--- src/components/Questions/Questions.tsx
+++ src/components/Questions/Questions.tsx
@@ -7,36 +7,34 @@ const Questions = () => {
 		<div className={styles.root}>
 			<span className={styles.leaves} />
 			<span className={styles.have} />
-			<div className={styles.back}>
-				<Container>
-					<div>
-						<div className={styles.head}>
-							<h1 className={styles.title}>
-								остались <span>вопросы?</span>
-							</h1>
-							<p className={styles.text}>
-								<span className={styles.line} />
-								Отправьте ваш вопрос, заказ, предложение или жалобу через форму
-								обратной связи, и наш специалист свяжется с вами в течение 15
-								минут.
-							</p>
-						</div>
-						<div className={styles.form}>
-							<form action='submit'>
-								<input type='text' placeholder={'Ваше имя'} />
-								<input type='tel' placeholder={'+7 (977) 777-77-77'} />
-								<input type='text' placeholder={'Ваш комментарий'} />
-								<button type={'submit'}>отправить</button>
-								<p>
-									Нажимая на кнопку «Отправить», я даю свое согласие на
-									обработку персональных данных, в соответствии с{' '}
-									<Link to={'/'}>Политикой конфиденциальности</Link>
-								</p>
-							</form>
-						</div>
+			<Container>
+				<div>
+					<div className={styles.head}>
+						<h1 className={styles.title}>
+							остались <span>вопросы?</span>
+						</h1>
+						<p className={styles.text}>
+							<span className={styles.line} />
+							Отправьте ваш вопрос, заказ, предложение или жалобу через форму
+							обратной связи, и наш специалист свяжется с вами в течение 15
+							минут.
+						</p>
 					</div>
-				</Container>
-			</div>
+					<div className={styles.form}>
+						<form action='submit'>
+							<input type='text' placeholder={'Ваше имя'} />
+							<input type='tel' placeholder={'+7 (977) 777-77-77'} />
+							<input type='text' placeholder={'Ваш комментарий'} />
+							<button type={'submit'}>отправить</button>
+							<p>
+								Нажимая на кнопку «Отправить», я даю свое согласие на обработку
+								персональных данных, в соответствии с{' '}
+								<Link to={'/'}>Политикой конфиденциальности</Link>
+							</p>
+						</form>
+					</div>
+				</div>
+			</Container>
 		</div>
 	)
 }
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -5,6 +5,7 @@ import Popular from '../../components/Popular/Popular'
 import HowDoOrder from '../../components/HowDoOrder/HowDoOrder'
 import Special from '../../components/Special/Special'
 import Questions from '../../components/Questions/Questions'
+import Inst from '../../components/Inst/Inst'
 
 const Main = () => {
 	return (
@@ -15,6 +16,7 @@ const Main = () => {
 			<HowDoOrder />
 			<Special />
 			<Questions />
+			<Inst />
 		</div>
 	)
 }
