8a46e6b80c73730b4d5cb1c0d1faf11b3e80acff
Kan <kanyakancat@gmail.com>
2024-01-18 19:23:45 +0300

    grid header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Routes } from 'react-router-dom'
 import Header from './components/Header/Header'
-import Main from './pages/Main/Main'
-import Footer from './components/Footer/Footer'
 
 function App() {
 	return (
 		<>
-			<Header isMain={true} />
-			<Main />
-			<Footer />
+			<Header isMain={false} />
+			{/*<Main />*/}
+			{/*<Footer />*/}
 			<Routes>{/*<Route path='/' element={<Header />} />*/}</Routes>
 		</>
 	)
